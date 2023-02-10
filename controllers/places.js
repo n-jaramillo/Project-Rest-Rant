0366cc123ac763fa6bda9b860111b51d4f33449b
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-10 02:26:50 -0800

    part 5.6 refactored places data

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/qE1jxYXiwOA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coding-cat-cafe.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/fyQr1T3GE34?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    }]
-
     res.render('places/index', { places })
 })
 
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/qE1jxYXiwOA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coding-cat-cafe.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/fyQr1T3GE34?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+}]
