dca44a21d6e01f3b9bcb516f6ed6710da7193978
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-09 17:38:36 -0800

    part 4.5 added an image to 404 view

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src='/images/404.jpg' alt='Spilled Ice Cream Cone on Concrete' className='img-fluid'/>
+                    Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/photos/52jRtc2S_VE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
