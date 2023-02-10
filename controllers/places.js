153f7c997558ea419b4f3757f3091842910b5978
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-09 17:52:37 -0800

    part 4.6 added content and bootstrap grid to index view

--- controllers/places.js
+++ controllers/places.js
@@ -6,13 +6,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/h-thai-ml.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/qE1jxYXiwOA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/coding-cat-cafe.jpg' // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/fyQr1T3GE34?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     }]
 
     res.render('places/index', { places })
