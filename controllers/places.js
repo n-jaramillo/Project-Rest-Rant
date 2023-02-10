e702c31276c27fe74e49f990a3e1d2d609081d5c
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-10 02:22:08 -0800

    part 5.5 added body parser to index.js and router.post in places.js

--- controllers/places.js
+++ controllers/places.js
@@ -18,6 +18,11 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+})
+
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
