8f6c441779f08091ad222a5b12ebe4cc448c9ec3
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-16 15:04:29 -0800

    part 8.3: updated index and show routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,5 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
-
+// const places = require('../models/places.js')
+/*
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
@@ -84,6 +83,59 @@ router.put('/:id', (req, res) => {
         places[id] = req.body
         res.redirect(`/places/${id}`)
     }
+})
+*/
+const router = require('express').Router()
+const db = require('../models')
+
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+        res.render('places/index', { places })
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error404')
+    })
+})
+
+router.post('/', (req, res) => {
+    db.Place.create(req.body)
+    .then(() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error404')
+    })
+})
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+    res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+    res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+    res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
