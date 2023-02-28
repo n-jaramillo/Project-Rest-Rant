1a8180278281996d9e62078262fa7c502b077201
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-27 23:17:39 -0800

    part 9 bonus: added custom error messages for new view

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
+function new_form(data) {
+    let message = ''
+    if (data.message) {
+        message = (
+            <h4 className='alert-danger'>
+                {data.message}
+            </h4>
+        )
+
+    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form action='/places' method='POST'>
                     <div className='row'>
                         <div className='form-group col-sm-6 col-md-4 col-lg-3'>
