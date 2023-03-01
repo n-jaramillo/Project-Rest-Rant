e7185964a3ae384162f9c60e9a116bdbe511e7ff
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-01 10:50:02 -0800

    part 10.1: created the comment model and referenced in place model

--- models/places.js
+++ models/places.js
@@ -1,24 +1,8 @@
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/h-thai-ml.jpg', // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/qE1jxYXiwOA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//     founded: '1994'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/coding-cat-cafe.jpg', // imgCredit: Photo by <a href="https://unsplash.com/@sevcovic23?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Petr Sevcovic</a> on <a href="https://unsplash.com/photos/fyQr1T3GE34?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-//     founded: '1991'
-// }]
-
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    pic: { type: String, default: '/images/chia-fruit-drink.jpg'},
+    pic: { type: String, default: '/images/chia-fruit-drink.jpg' },
     cuisines: { type: String, required: true },
     city: { type: String, default: 'Anytown' },
     state: { type: String, default: 'USA' },
@@ -26,7 +10,8 @@ const placeSchema = new mongoose.Schema({
         type: Number,
         min: [1673, 'Surely not that old?!'],
         max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-    }
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
 placeSchema.methods.showEstablished = function () {
